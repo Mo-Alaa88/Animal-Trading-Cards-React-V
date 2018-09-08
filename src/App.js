b893a30c509f8437d56b2a97767670f664e9689c
Mohammed Alaa <38194603+Mo-Alaa88@users.noreply.github.com>
2018-09-09 01:06:57 +0200

    Add Image & Edit code App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fish from './image/design-prototype.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div id="background" className="animal-cards">
+          <header>
+            <h3 className="animal-name">Nemo Fish</h3>
+          </header>
+          <img src={fish} class="cat-picture" alt="fish"/>
+          <div className="cat-info">
+            <p className="interesting-fact">A layer of mucus on the clownfish`s skin make it immune to the fish-eating aneone`s lethal sting.</p>
+
+            <ul class="list-item">
+              <li><span>scientific name</span> : Amphipioninae</li>
+              <li><span>average length</span> : 11 centimeters.</li>
+              <li><span>average lifespan</span> :7 years.</li>
+              <li><span>habitat</span> : Tropical.</li>
+            </ul>
+            <p className="description">Bright orange with three distinctive with bars , clown anemonefish are among the most recognizable of all reffdwellers They reach about 11 centimeters in length , and are named for the multicolored sea anemone in which they make their homes.</p>
+          </div>
+        </div>
+        <footer>Created By Mohammed Alaa <a href="https://github.com/Mo-Alaa88" target="_blank">See More Projects</a></footer>
       </div>
     );
   }
